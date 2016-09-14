ad120cf53a2474e9baad5ee80fea41a1a0c6f147
Xionghui Li <lixionghui@gmail.com>
2016-09-14 09:18:10 +0800

    Update httrGetSymbols.netease.R

--- httrGetSymbols.netease.R
+++ httrGetSymbols.netease.R
@@ -1,28 +1,8 @@
 httrGetSymbols.netease("002578")
 
 code <- "002578"
-market <- NA
+market <- "SZ"
 
-# Make code into 7 digits if it's not
-if (nchar(code) < 6) {
-  code <- paste(c(rep(0, 6-nchar(code)), code), collapse="")
-}
-if (nchar(code)==7) {
-  market <- substr(code, 1, 1)
-  code <- substr(code, 2, 7)
-}
-if (nchar(code) == 6) {
-  if (missing("market")) {
-    market <- ifelse(substr(code, 1, 1)<5, "SZ", "SH")
-  } else if (market %in% c("0", "Shanghai", "SH", "sh")) {
-    market <- "SH"
-  } else if (market %in% c("1", "Shenzhen", "SZ", "sz")) {
-    market <- "SZ"
-  } else {
-    stop("Cannot recognize market")
-  }
-}
-# codename is for the name of data frame and file to save.
 # codenum is for the link to netease.
 codename <- paste("CODE", code, sep="")
 codenum <- paste(ifelse(market=="SH", 0, 1), code, sep="")
@@ -42,3 +22,13 @@ link <- paste("http://quotes.money.163.com/service/chddata.html?",
               "end=", to, "&",
               "fields=", paste(fields, collapse=";"),
               sep="")
+
+library(httr)
+library(magrittr)
+
+r <- GET(link)
+
+GET(link) %>%
+  content(type = "text", encoding = "gb18030") %>%
+  writeBin("myfile.csv")
+  cat() %>%
