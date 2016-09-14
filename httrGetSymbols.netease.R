f9132d96e2935621e1f31fb15392c8dfb26765d7
Xionghui Li <lixionghui@gmail.com>
2016-09-14 13:03:54 +0800

    Update httrGetSymbols.netease.R

--- httrGetSymbols.netease.R
+++ httrGetSymbols.netease.R
@@ -1,4 +1,3 @@
-httrGetSymbols.netease("002578")
 
 code <- "002578"
 market <- "SZ"
@@ -8,7 +7,7 @@ codename <- paste("CODE", code, sep="")
 codenum <- paste(ifelse(market=="SH", 0, 1), code, sep="")
 
 
-from <- "20060101"
+from <- "20160101"
 to <- format(Sys.Date(), format="%Y%m%d")
 
 fields=c("TCLOSE", "HIGH", "LOW", "TOPEN",
@@ -23,12 +22,35 @@ link <- paste("http://quotes.money.163.com/service/chddata.html?",
               "fields=", paste(fields, collapse=";"),
               sep="")
 
+# fields # 字段中文名和含义
+# TCLOSE # 收盘价
+# HIGH # 最高价
+# LOW # 最低价
+# TOPEN # 开盘价
+# LCLOSE # 前收盘
+# CHG # 涨跌额
+# PCHG # 涨跌幅
+# TURNOVER # 换手率
+# VOTURNOVER # 成交量
+# VATURNOVER # 成交金额
+# TCAP # 总市值
+# MCAP # 流通市值
+
+
 library(httr)
 library(magrittr)
+library(stringr)
 
-r <- GET(link)
 
-GET(link) %>%
+GET(link) %>% 
   content(type = "text", encoding = "gb18030") %>%
-  writeBin("myfile.csv")
-  cat() %>%
+  str_split("\\r\\n") %>% 
+  unlist() %>% # 将列表转为向量
+  extract(2:length(.)-1) %>% # 第一行是字段名，最后一个行是空字符 
+  str_split(",", simplify = TRUE) %T>%
+
+  
+  
+  
+  
+  
